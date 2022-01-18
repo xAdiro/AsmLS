f099723b86de745cb07307df1c28ae98a4596f92
Adiro <adiro.rostek@gmail.com>
2022-01-18 02:00:11 +0100

    first commit

---
+++ nasm_ide/src/main/java/com/adiro/nasm_ide/SystemInfo.java
@@ -0,0 +1,13 @@
+package com.adiro.nasm_ide;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
