f10ec0c8af91e3c09c4a94c4e188a2092fb3f7a5
Adiro <adiro.rostek@gmail.com>
2022-01-23 00:54:17 +0100

    refactored basically everything. Doesn't look like car accident anymore

--- nasm_ide/src/main/java/com/adiro/nasm_ide/App.java
+++ nasm_ide/src/main/java/com/adiro/nasm_ide/App.java
@@ -1,10 +1,10 @@
 package com.adiro.nasm_ide;
 
 import java.io.IOException;
-
+import com.adiro.nasm_ide.gui.content.ContentView;
+import com.adiro.nasm_ide.gui.menu.IconMenuBar;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -19,19 +19,9 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-    	var	sourceCode = new SourceViewArea();
-    	var registers = new RegistersView();
-    	var	reader = new LogReader(registers, sourceCode);
-    	var nextButton = new MoveNextButton(sourceCode, reader);  	
-    	
-    	var fileButton = new changeFileButton(reader, stage);
-    	var sourceButton = new SourceButton(sourceCode, stage);
-    	
-    	
-    	var column1 = new VBox(sourceCode, nextButton, fileButton, sourceButton);
-    	var column2 = new VBox(registers);
-    	
-    	var layout = new HBox(column1, column2);
+    	var contentView = new ContentView();    	
+    	var topMenu = new IconMenuBar(contentView, stage);
+    	var layout = new VBox(topMenu, contentView);
 
         var scene = new Scene(new StackPane(layout), 1366, 768);
         stage.setScene(scene);
